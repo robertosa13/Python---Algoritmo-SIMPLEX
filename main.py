c7cfba97efe1987777f115d2bed233fe36b95510
Roberto Sá Barreto <90226923+robertosa13@users.noreply.github.com>
2021-09-08 14:40:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,73 @@
+Created by Roberto Sá on 26/04/2021
+
+import random
+import pulp as plp
+import pandas as pd
+
+
+
+#PASSO 1: DEFINIR A DIMENSÃO DAS MATRIZES
+n = 1240
+m = 5000
+
+#PASSO 2: ALOCAR COEFICIENTE PARA AS MATRIZES
+set_I = range(1, n+1)
+set_J = range(1, m+1)
+c = {(i,j): random.normalvariate(0,50) for i in set_I for j in set_J}
+a = {(i,j): random.normalvariate(0,1) for i in set_I for j in set_J}
+l = {(i,j): random.randint(0,1) for i in set_I for j in set_J}  #low
+u = {(i,j): random.randint(0,1) for i in set_I for j in set_J} #upper
+b = {j: random.randint(0,1) for j in set_J}                     #b
+
+
+#PASSO 3: CRIO O 'OBJETO' PARA SER OTIMIZADO
+opt_model = plp.LpProblem(name="MIP Model")
+
+#PASSO 4: CÓDIGO ABAIXO DEFINIÇÃO DO VALOR DE CADA VARIÁVEL
+
+x_vars  = {(i,j):
+plp.LpVariable(cat=plp.LpBinary, name="x_{0}_{1}".format(i,j))
+for i in set_I for j in set_J}
+
+#PASSO 5 DEFINIR O SINAL " <  >  <=   >=  = "
+
+# >= constraints
+constraints = {j : opt_model.addConstraint(
+plp.LpConstraint(
+             e=plp.lpSum(a[i,j] * x_vars[i,j] for i in set_I),
+             sense=plp.LpConstraintGE,
+             rhs=b[j],
+             name="constraint_{0}".format(j)))
+       for j in set_J}
+
+
+#PASSO 6: MONTAR A LINHA OBJETIVO
+
+objective = plp.lpSum(x_vars[i,j] * c[i,j]
+                    for i in set_I
+                    for j in set_J)
+
+#PASSO 7: DEFINIR SE O PROBLEMA É MAXIMIZAÇÃO OU MINIMIZAÇÃO
+
+# for maximization
+opt_model.sense = plp.LpMaximize
+
+opt_model.setObjective(objective)
+
+
+#PASSO 8: SOLUCIONAR O PROBLEMA(SOLVER)
+# solving with CBC
+opt_model.solve()
+# solving with Glpk
+#opt_model.solve(solver = GLPK_CMD())
+
+
+#PASSO 9: UTILIZAR A BIBLIOTECA PANDAS PARA O PÓS PROCESSAMENTO
+
+opt_df = pd.DataFrame.from_dict(x_vars, orient="index",columns = ["variable_object"])
+opt_df.index = pd.MultiIndex.from_tuples(opt_df.index,names=["column_i", "column_j"])
+opt_df.reset_index(inplace=True)
+# PuLP
+opt_df["solution_value"] = opt_df["variable_object"].apply(lambda item: item.varValue)
+opt_df.drop(columns=["variable_object"], inplace=True)
+opt_df.to_csv("./optimization_solution.csv")
